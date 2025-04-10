7ad2fa198b1f0fbf1eac1ce42d1debd0f8a98084
Shivam Garade <shivamgarade05@gmail.com>
2025-04-10 13:46:11 +0530

    break

---
+++ Lecture 5/script.js
@@ -0,0 +1,39 @@
+//group of characters covered with the string
+// people says string is a arrays of characters -- but its not true -- it behaves like a array
+// let s = "shivam";
+// console.log(s)
+//string and array dono me indexing hoti hai 
+//strings are immutable in nature but can be "reassign"
+ // slice cover negative part also but substring not 
+//tum jo kar rahe ho "s." ye string me change nahi kar rahe tum original string ko "use" kar rahe ho .
+
+
+// let arr = [10,20,30,40,50]
+// arr.push(60)
+// console.log(arr.length)
+// console.log(arr[2])
+
+// let s = 'Sheryians'
+// //s.push(1)
+// s = s + " World"
+// console.log(s)
+
+//Method in string :
+  let s = "Sheryians"
+//   console.log(s.length) // 9   
+// console.log(s.slice(1,5)) //hery 
+// console.log(s.slice(-4,s.length)) //ians
+// console.log(s.substring(2)); //eryians
+// console.log(s.toUpperCase()); //SHERYIANS
+// console.log(s.toUpperCase()); //sheryians
+// console.log(s.concat(" ","World")); //sheryians World
+
+// let s_trim = "             shivam                "
+// console.log(s_trim.trim())// shivam
+
+console.log(s.charAt(2)) //e
+
+//ASCII VALUES
+// A - 65 , B - 66
+// a - 97 , b - 98
+// 0 - 48 , 1 - 49
