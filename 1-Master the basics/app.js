05960304dc5d2ea78bbe84bdaf2afa4a8afe5fd6
Shivam Garade <shivamgarade05@gmail.com>
2025-05-21 14:43:41 +0530

    Lecture rewatched completly

---
+++ 1-Master the basics/app.js
@@ -0,0 +1,28 @@
+console.log(Math.round(10.9));
+console.log(Math.ceil(10.1));
+console.log(Math.floor(10.9));
+console.log(Math.trunc(10.23443));
+console.log(Math.pow(2,4));
+console.log(Math.sqrt(69));
+console.log(Math.abs(-12));
+console.log(Math.max(12,34));
+console.log(Math.min(12,34));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*9000+1000));
+// let a = 12.2344
+// console.log(a.toFixed(2));
+// let b = 12;
+// let l = 12;
+// console.log(l*b);
+
+let a = 5;
+let b = 3;
+let c = 4;
+
+let s = (a+b+c)/2
+console.log(Math.sqrt(s * (s-a) * (s -b) * (s - c)));
+
+
+
+let r = 12;
+console.log(Number((2 * Math.PI * r).toFixed(2)));
